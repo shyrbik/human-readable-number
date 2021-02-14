565786c172b669559c986ae7e2602b40287aa60d
shyrbik <58969166+shyrbik@users.noreply.github.com>
2021-02-14 11:16:17 +0300

    feat: add my own code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+    let resultString = "";
+    let arrayForRead = numberToArray(number);
+    let numberToWordArray = ["zero", "one", "two", "three", "four", "five","six", "seven", "eight", "nine"];
+
+    for (let i = arrayForRead.length-1; i > 0; i--) {
+		
+        console.log(arrayForRead[i])
+        resultString += numberToWordArray[arrayForRead[i]] + " ";
+
+    }
+
+    return resultString.trim();
+}
+
+function numberToArray (numberForArray){
+    let arrayForRead = [];
+    let i = 0;
+    while (numberForArray > 1) {
+        arrayForRead[i] = numberForArray%10;
+        i++;
+    numberForArray = (numberForArray - numberForArray%10) / 10;
+       if (numberForArray < 10) {
+           arrayForRead[i] = numberForArray;
+       }
+    }
+
+    return arrayForRead;
 }
